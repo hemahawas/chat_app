5d5fb376c411ab63805165faed7fe6e631ca132f
HemaHawas <hawas711@gmail.com>
2024-10-24 09:09:56 +0300

    modified the profile separating line

--- lib/features/home/presentaion/view/widgets/separating_line.dart
+++ lib/features/home/presentaion/view/widgets/separating_line.dart
@@ -7,16 +7,13 @@ class SeparatingLine extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 1,
-      child: Flex(direction: Axis.vertical, children: [
-        Expanded(
-          child: OverflowBox(
-            maxWidth: MediaQuery.sizeOf(context).width,
-            child: Container(
-              color: Colors.grey,
-            ),
-          ),
+      child: OverflowBox(
+        maxHeight: 1,
+        maxWidth: MediaQuery.sizeOf(context).width,
+        child: Container(
+          color: Colors.grey,
         ),
-      ]),
+      ),
     );
   }
 }
