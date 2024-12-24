c4aadb2b4131a71062bb679eb9ab7538ea3859e1
HemaHawas <hawas711@gmail.com>
2024-12-24 09:02:43 +0200

    modified the message item ui

--- lib/features/messaging/presentation/view/widgets/message_item.dart
+++ lib/features/messaging/presentation/view/widgets/message_item.dart
@@ -35,71 +35,71 @@ class MessageItem extends StatelessWidget {
               )
             : Container(),
         ConstrainedBox(
-          constraints: BoxConstraints(minHeight: 50),
-          child: Flexible(
-            fit: FlexFit.tight,
-            child: Container(
-              height: 80,
-              width: double.infinity,
-              padding: EdgeInsets.all(8),
-              margin: currentUserUId == message.messageSenderId
-                  ? EdgeInsets.only(
-                      top: 1,
-                      bottom: 1,
-                      left: MediaQuery.of(context).size.width * 0.25,
-                      right: 5)
-                  : EdgeInsets.only(
-                      top: 1,
-                      bottom: 1,
-                      right: MediaQuery.of(context).size.width * 0.25,
-                      left: 5),
-              alignment: Alignment.topRight,
-              decoration: BoxDecoration(
-                  borderRadius: currentUserUId == message.messageSenderId
-                      ? BorderRadius.circular(10.0)
-                          .copyWith(topRight: Radius.zero)
-                      : BorderRadius.circular(10.0)
-                          .copyWith(topLeft: Radius.zero),
-                  color: currentUserUId == message.messageSenderId
-                      ? ColorApp.messageBodyOfCurrentUserColor
-                      : ColorApp.messageBodyOfOtherUserColor),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  isGroup
-                      ? Align(
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            currentUserUId == message.messageSenderId
-                                ? ''
-                                : '${participantNames[message.messageSenderId]}',
-                            style: Styles.textStyle15,
-                          ),
-                        )
-                      : Container(),
-                  Container(
-                    child: message.image != null
-                        ? Image.network(message.image!)
-                        : null,
-                  ),
-                  Text(
-                    message.body == null
-                        ? ''
-                        : (message.body == 'Photo.' ? '' : message.body!),
-                    maxLines: 6,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      message.sendingTime != null
-                          ? '${message.sendingTime!.hour}:${message.sendingTime!.minute}'
-                          : '',
-                      style: Styles.textStyle15
-                          .copyWith(fontSize: 12.0, color: Colors.grey),
+          constraints: BoxConstraints(minHeight: 30),
+          child: Container(
+            height: message.image != null ? 200 : 60,
+            width: double.infinity,
+            padding: EdgeInsets.all(8),
+            margin: currentUserUId == message.messageSenderId
+                ? EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.25, right: 5)
+                : EdgeInsets.only(
+                    right: MediaQuery.of(context).size.width * 0.25, left: 5),
+            alignment: Alignment.topRight,
+            decoration: BoxDecoration(
+                borderRadius: currentUserUId == message.messageSenderId
+                    ? BorderRadius.circular(10.0)
+                        .copyWith(topRight: Radius.zero)
+                    : BorderRadius.circular(10.0)
+                        .copyWith(topLeft: Radius.zero),
+                color: currentUserUId == message.messageSenderId
+                    ? ColorApp.messageBodyOfCurrentUserColor
+                    : ColorApp.messageBodyOfOtherUserColor),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                isGroup && currentUserUId != message.messageSenderId
+                    ? Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          '${participantNames[message.messageSenderId]}',
+                          style: Styles.textStyle15,
+                        ),
+                      )
+                    : Container(
+                        child: null,
+                      ),
+                Container(
+                  child: message.image != null
+                      ? Image.network(message.image!)
+                      : null,
+                ),
+                Flexible(
+                  child: Row(children: [
+                    LimitedBox(
+                      maxWidth: MediaQuery.of(context).size.width * 0.6,
+                      child: Text(
+                        message.body == null
+                            ? ''
+                            : (message.body == 'Photo.' ? '' : message.body!),
+                        maxLines: 6,
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
                     ),
-                  )
-                ],
-              ),
+                    Spacer(),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        message.sendingTime != null
+                            ? '${message.sendingTime!.hour}:${message.sendingTime!.minute}'
+                            : '',
+                        style: Styles.textStyle15
+                            .copyWith(fontSize: 12.0, color: Colors.grey),
+                      ),
+                    )
+                  ]),
+                ),
+              ],
             ),
           ),
         ),
