651ee1b1070296aa2c3ad8b177a6603043cc3036
HemaHawas <hawas711@gmail.com>
2024-12-29 10:59:09 +0200

    Added group info to view members

---
+++ lib/features/group/presentation/view/widgets/group_member_item.dart
@@ -0,0 +1,39 @@
+import 'package:chat_app/core/themes/styles.dart';
+import 'package:chat_app/core/utils/user_model.dart';
+import 'package:chat_app/features/home/presentation/view/widgets/image_field.dart';
+import 'package:chat_app/features/home/presentation/view/widgets/user_item.dart';
+import 'package:flutter/material.dart';
+
+class GroupMemberItem extends StatelessWidget {
+  const GroupMemberItem({super.key, required this.model});
+  final UserModel model;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ImageField(
+          borderColor: Colors.transparent,
+          size: 50,
+          image: model.image,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              model.name!,
+              style: Styles.textStyle24.copyWith(fontSize: 12),
+            ),
+            Text(
+              model.phone!,
+              style:
+                  Styles.textStyle15.copyWith(fontSize: 12, color: Colors.grey),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
